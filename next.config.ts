486f63286dfd2f710c40e87416f7a75188ea3955
DevRipley <rykaalis@gmail.com>
2024-12-17 02:28:04 -0400

    next c

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Define que se utilizará la exportación estática
+  distDir: 'out',   // Especifica que los archivos generados se guardarán en el directorio 'out'
 };
 
 export default nextConfig;
